99c3a976ede128f71415f2569b7cb6327d5f1207
adrocx <131354760+adrocx@users.noreply.github.com>
2023-07-29 09:51:39 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "ADROCX"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Aditya"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ADX"
 )
 
 var ID ids.ID
